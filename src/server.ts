10dc165c2b31fe366f8cb76b85fae6ba2d229c3a
oopss900 <mohddilshad3743@gmail.com>
2025-11-20 11:41:58 +0530

    changes code

--- src/server.ts
+++ src/server.ts
@@ -10,10 +10,27 @@ const app = express();
 
 app.use(express.json())
 
-app.use(cors({
-    origin: process.env.ORIGIN_CORS || "http://localhost:4200",
-    Credential: true
-}))
+// app.use(cors({
+//     origin: process.env.ORIGIN_CORS || "https://angular-backend-ten.vercel.app",
+//     Credential: true
+// }))
+
+app.use(
+    cors({
+        origin: (origin, callback) => {
+            const allowedOrigins = [
+                "http://localhost:4200",
+                "https://token-implement-frontend.vercel.app"
+            ];
+            if (!origin || allowedOrigins.includes(origin)) {
+                callback(null, true);
+            } else {
+                callback(new Error("Not allowed by CORS"));
+            }
+        },
+        credentials: true,
+    })
+);
 
 //database connection
 if (process.env.MONGO_URL) {
@@ -24,7 +41,7 @@ else {
     process.exit(1)
 }
 
-app.use("/api/auth",routes)
+app.use("/api/auth", routes)
 
 const port = 5000;
 app.listen(port, () => console.log("server running!"))
