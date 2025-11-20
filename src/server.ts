2244b2b1ba126ef796bb042db1beebd5ccdd6282
oopss900 <mohddilshad3743@gmail.com>
2025-11-20 13:17:57 +0530

    changes

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,19 @@
-import dotenv from 'dotenv'
-dotenv.config()
-import express from 'express'
-import cors from 'cors'
+import dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
+import cors from 'cors';
 import { DBConnection } from './config/database';
-import routes from './routes'
-
+import routes from './routes';
 
 const app = express();
-
-app.use(express.json())
-
-// app.use(cors({
-//     origin: process.env.ORIGIN_CORS || "https://angular-backend-ten.vercel.app",
-//     Credential: true
-// }))
+app.use(express.json());
 
 app.use(
     cors({
-        origin: (origin: any, callback: any) => {
+        origin: (origin, callback) => {
             const allowedOrigins = [
                 "http://localhost:4200",
-                "https://angular-learning-ruddy.vercel.app/"
+                "https://angular-learning-ruddy.vercel.app"
             ];
             if (!origin || allowedOrigins.includes(origin)) {
                 callback(null, true);
@@ -32,17 +25,15 @@ app.use(
     })
 );
 
-//database connection
+// Connect DB
 if (process.env.MONGO_URL) {
-    DBConnection()
-}
-else {
+    DBConnection();
+} else {
     console.log("MongoDB connection string is missing in .env");
-    process.exit(1)
+    process.exit(1);
 }
 
-app.use("/api/auth", routes)
+app.use("/api/auth", routes);
 
-const port = 5000;
-app.listen(port, () => console.log("server running!"))
-// app.listen(5000,"this is")
+// For Vercel
+export default app;
